ecca6fb8fb12149dd3898db2014274ee850f2fd6
GabrielHCustodio <93563385+GabrielHCustodio@users.noreply.github.com>
2022-04-02 22:06:37 -0300

    This is my commit practicing the V-for directive.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,47 @@
 const MyNameApp = {
     data() {
         return {
-            // elementosHTML: `<p>Este é o <br>título </br> do meu teste</p> <a href="https://www.google.com">Google</a>`
-            saldo: 1100
+            cursos: ['Vue', 'React', 'Angular', 'Java', 'Node', 'PHP'],
+            cursos2: {
+                1000: {
+                    title: "Vue Js",
+                    description: "Aprenda um dos frameworks mais utilizados do momento"
+                },
+                1001: {
+                    title: "Node Js",
+                    description: "Aprenda a ferramenta Back-end mais utilizado do momento"
+                }
+            },
+            publicacoes: [
+                { id: 1, publicacao: "Publicação 1"},
+                { id: 2, publicacao: "Publicação 2"},
+                { id: 3, publicacao: "Publicação 3"}
+            ],
+            comentarios: [
+                { id: 1, comentario: "Comentário 1"},
+                { id: 2, comentario: "Comentário 2"},
+                { id: 3, comentario: "Comentário 3"}
+            ]
         }
     },
     methods: {
-        debitar() {
-            this.saldo -= parseFloat(valor.value)
+        misturarPubli() {
+            let misturado = []
+
+            misturado[0] = this.publicacoes[2]
+            misturado[1] = this.publicacoes[0] 
+            misturado[2] = this.publicacoes[1]
+
+            this.publicacoes = misturado
         },
-        creditar() {
-            this.saldo += parseFloat(valor.value)
+        adicionarElemento() {
+            this.comentarios.push({ id: 4, comentario: "Comentário adicionado"})
+        },
+        removerElemento() {
+            this.comentarios.pop()
+        },
+        modificarElemento() {
+            this.comentarios[1] = { id: 2, comentario: "Comentário 2 atualizado"}
         }
     }
 }
