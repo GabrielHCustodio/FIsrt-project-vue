2ef83e66217d057f1d208a98624a8b7f7ef9eb85
GabrielHCustodio <93563385+GabrielHCustodio@users.noreply.github.com>
2022-04-04 21:54:19 -0300

    This is my commit practicing computed property.

--- src/app.js
+++ src/app.js
@@ -1,47 +1,31 @@
 const MyNameApp = {
     data() {
         return {
-            cursos: ['Vue', 'React', 'Angular', 'Java', 'Node', 'PHP'],
-            cursos2: {
-                1000: {
-                    title: "Vue Js",
-                    description: "Aprenda um dos frameworks mais utilizados do momento"
-                },
-                1001: {
-                    title: "Node Js",
-                    description: "Aprenda a ferramenta Back-end mais utilizado do momento"
-                }
-            },
-            publicacoes: [
-                { id: 1, publicacao: "Publicação 1"},
-                { id: 2, publicacao: "Publicação 2"},
-                { id: 3, publicacao: "Publicação 3"}
-            ],
-            comentarios: [
-                { id: 1, comentario: "Comentário 1"},
-                { id: 2, comentario: "Comentário 2"},
-                { id: 3, comentario: "Comentário 3"}
-            ]
+            pacientes: []
         }
     },
     methods: {
-        misturarPubli() {
-            let misturado = []
+        adicionarPacientes() {
+            this.pacientes.push({
+                nome: inputNome.value,
+                idade: inputIdade.value,
+                plano: inputPlano.value
+            })
+        }
+    },
+    computed: {
+        ultimoPacienteAdicionado() {
+            let key = this.pacientes.length - 1
+            let txt = ''
 
-            misturado[0] = this.publicacoes[2]
-            misturado[1] = this.publicacoes[0] 
-            misturado[2] = this.publicacoes[1]
+            txt += 'Paciente ' + this.pacientes[key].nome
+            txt += ', idade ' + this.pacientes[key].idade
+            txt += ', plano ' + this.pacientes[key].plano
 
-            this.publicacoes = misturado
+            return txt
         },
-        adicionarElemento() {
-            this.comentarios.push({ id: 4, comentario: "Comentário adicionado"})
-        },
-        removerElemento() {
-            this.comentarios.pop()
-        },
-        modificarElemento() {
-            this.comentarios[1] = { id: 2, comentario: "Comentário 2 atualizado"}
+        pacientePlanoOuro() {
+            return this.pacientes.filter( items => items.plano == 'Ouro')
         }
     }
 }
