f273dfb03a717d7196a9934ce68c5b849117bf6f
GabrielHCustodio <93563385+GabrielHCustodio@users.noreply.github.com>
2022-04-06 21:17:44 -0300

    This is my commit practicing watch property.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 const MyNameApp = {
     data() {
         return {
-            paciente: 'Gabriel Henrique',
-            sexo: '',
-            plano: '',
-            doencas: [],
-            maisInformacoes: ''
+            paciente: '',
+            pacientes: [
+                {nome: 'Gabriel Henrique', idade: 21},
+                {nome: 'Guilherme Siqueira', idade: 46},
+                {nome: 'Gabriela Candido', idade: 27},
+                {nome: 'Gabriel Marques', idade: 52},
+                {nome: 'Guilherme Henrique', idade: 84}
+            ],
+            lista: []
         }
     },
     methods: {},
-    computed: {}
+    computed: {},
+    watch: {
+        paciente(valorNovo) {
+            this.lista = this.pacientes.filter( p => p.nome.match(valorNovo))
+        }
+    }
 }
 
 Vue.createApp(MyNameApp).mount("#app")
